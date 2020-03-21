5e766dbc7ebf7dd224651391daa5a9084abf5be5
Brendon Boldt <brendon.boldt@gmail.com>
2020-03-21 00:35:48 -0400

    Turn vMF loss into torch Module

--- loss.py
+++ loss.py
@@ -5,66 +5,57 @@ from torch import Tensor
 import numpy as np
 import torch
 
-"""
-vMF distribution
-p(e(w); µ, κ) = C_m(κ) * exp(κ * (µ @ e(w)))
 
-p(e_tgt; e_pred) = vMF(e_tgt); e_pred) = C_m(norm(e_pred)) * exp(e_pred @ e_tgt)
-NLLvMF = - log(C_m(norm(e_pred))) - e_pred @ e_tgt
-"""
+class VonMisesFisherLoss(torch.nn.modules.loss._Loss):
+    """Loss according to a von Mises Fisher distribution
 
-LOG_2_PI = Tensor([math.log(math.tau)])
-
-
-def calculate_bessel_consts(v: float, n_iters: int) -> Tuple[Tensor, Tensor]:
-    bessel_coeffs = np.ndarray(n_iters)
-    for i in range(n_iters):
-        bessel_coeffs[i] = -math.log(math.factorial(i)) - math.lgamma(v + 1 + i)
-    bessel_coeffs = Tensor(bessel_coeffs)
-    bessel_exps = Tensor([2 * i + v for i in range(n_iters)])
-    return bessel_exps, bessel_coeffs
-
-
-def log_bessel(x: Tensor, bessel_consts: Tuple[Tensor, Tensor]) -> Tensor:
-    """Approximation of the log of modified Bessel function of the first kind
-
-    `bessel_consts` should be retrieved from `calculate_bessel_consts()`
-
+    PDF: p(e(w); µ, κ) = C_m(κ) * exp(κ * (µ @ e(w)))
+        where C_m = κ ** (m/2 - 1) / ((2 * pi) ** (m/2) I_(m/2 - 1)(κ))
+        where m is the dimension of κ
     """
 
-    return torch.logsumexp(torch.log(x / 2) * bessel_consts[0] + bessel_consts[1], 0)
+    LOG_2_PI = Tensor([math.log(math.tau)])
 
+    def __init__(self, input_dim: int, n_bessel_iters=10, reduction="mean") -> None:
+        super(VonMisesFisherLoss, self).__init__(reduction=reduction)
+        bessel_consts = VonMisesFisherLoss.calculate_bessel_consts(
+            input_dim, n_bessel_iters
+        )
+        self.bessel_exps, self.bessel_coeffs = bessel_consts
 
-def log_vmf_normalizing_const(
-    kappa: Tensor, m: Tensor, bessel_consts: Tuple[Tensor, Tensor]
-):
-    """Calculate the log normalizing constant C_m(kappa) for the vMF distribution
+    def forward(self, input: Tensor, target: Tensor) -> Tensor:
+        # TODO Add both types of regularization
+        x = (
+            -self.log_vmf_normalizing_const(input.norm(), input.shape[-1])
+            - 1e-1*(target @ input.T)
+        )
+        if self.reduction == 'none':
+            return x
+        elif self.reduction == 'mean':
+            return x.mean()
+        elif self.reduction == 'mean':
+            return x.sum()
 
-    C_m = κ ** (m/2 - 1) / ((2 * pi) ** (m/2) I_(m/2 - 1)(κ))
-    where m is the dimension of κ
+    def calculate_bessel_consts(v: float, n_iters: int) -> Tuple[Tensor, Tensor]:
+        bessel_coeffs = np.ndarray(n_iters)
+        for i in range(n_iters):
+            bessel_coeffs[i] = -math.log(math.factorial(i)) - math.lgamma(v + 1 + i)
+        bessel_coeffs = Tensor(bessel_coeffs)
+        bessel_exps = Tensor([2 * i + v for i in range(n_iters)])
+        return bessel_exps, bessel_coeffs
 
-    `bessel_consts` should be retrieved from `calculate_bessel_consts()`
-    """
+    def log_bessel(self, x: Tensor) -> Tensor:
+        """Approximation of the log of modified Bessel function of the first kind"""
 
-    return (
-        torch.log(kappa) * (m / 2 - 1)
-        - LOG_2_PI * m / 2
-        - log_bessel(kappa, bessel_consts)
-    )
+        return torch.logsumexp(
+            torch.log(x / 2) * self.bessel_exps + self.bessel_coeffs, 0
+        )
 
+    def log_vmf_normalizing_const(self, kappa: Tensor, m: Tensor) -> Tensor:
+        """Calculate the log normalizing constant C_m(kappa) for the vMF distribution"""
 
-"""
-vMF distribution
-p(e(w); µ, κ) = C_m(κ) * exp(κ * (µ @ e(w)))
-p(e_tgt; e_pred) = vMF(e_tgt); e_pred) = C_m(norm(e_pred)) * exp(e_pred @ e_tgt)
-NLLvMF = - log(C_m(norm(e_pred))) - e_pred @ e_tgt
-"""
-
-
-def nll_vmf(
-    e_pred: Tensor, e_tgt: Tensor, bessel_consts: Tuple[Tensor, Tensor]
-) -> Tensor:
-    return (
-        -log_vmf_normalizing_const(e_pred.norm(), e_pred.shape[-1], bessel_consts)
-        - e_tgt @ e_pred
-    )
+        return (
+            torch.log(kappa) * (m / 2 - 1)
+            - VonMisesFisherLoss.LOG_2_PI * m / 2
+            - self.log_bessel(kappa)
+        )
