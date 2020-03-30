eb64da63f64eec521a8d19e389302465920e4dc8
artidoro <pagnoni.artidoro@gmail.com>
2020-03-30 13:21:16 -0400

    move comment at end of command

--- scripts/vmf/train-vmf-enfr.sh
+++ scripts/vmf/train-vmf-enfr.sh
@@ -3,7 +3,6 @@ python main.py \
     --device cuda \
     --dropout 0.0 \
     --enc_bidirectional \
-#    --enc_num_layers 2\
     --fasttext_embeds_path ~/data/corpus.fasttext.fr \
     --input_feed \
     --loss_function vmf \
@@ -18,3 +17,5 @@ python main.py \
     --vmf_lambda_1 0.02 \
     --vmf_lambda_2 0.1 \
     --write_to_file \
+#    --enc_num_layers 2\
+
