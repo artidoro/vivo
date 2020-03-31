bb8977b70828f2aec78fea83ec684197d221888c
artidoro <pagnoni.artidoro@gmail.com>
2020-03-31 09:37:57 -0400

    evalall script

---
+++ scripts/eval_all.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+for filename in $1"/*.pt;" do
+    ./MyProgram.exe "$filename"
+    python main.py --checkpoint_path eval_all\
+               --src_language $2\
+               --trg_language $3\
+               --device cuda:$4\
+               --load_checkpoint_path $path$filename\
+               --use_checkpoint_args\
+               --unk_replace\
+               --mode eval\
+               -o mode\
+               -o unk_replace
+    echo "Results for checkpoint:\n"$path$filename
+done
