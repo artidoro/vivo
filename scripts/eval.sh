b208fb3c38973f899aae343c7175b5742908620b
artidoro <pagnoni.artidoro@gmail.com>
2020-03-31 09:57:59 -0400

    file name

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 python main.py\
-    --checkpoint_path "vmf-$1$2-eval-all"\
-    --src_language "$1"\
-    --trg_language "$2"\
-    --device "cuda:$3"\
-    --load_checkpoint_path "$path$filename"\
+    --checkpoint_path "vmf-$2$3-eval-all"\
+    --src_language "$2"\
+    --trg_language "$3"\
+    --device "cuda:$4"\
+    --load_checkpoint_path "$1"\
     --use_checkpoint_args\
     --unk_replace\
     --mode eval\
--- scripts/eval_all.sh
+++ scripts/eval_all.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for filename in $1"/*.pt;" do
-    /bin/bash "./eval.sh $2 $3 $4"
+    /bin/bash "./eval.sh $path$filename $2 $3 $4"
     echo "Results for checkpoint:\n"$path$filename
 done
