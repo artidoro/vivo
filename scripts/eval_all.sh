abdf6180ab2ec9e9d81c2c35a14ef81fbaf4ac91
artidoro <pagnoni.artidoro@gmail.com>
2020-03-31 09:49:29 -0400

    use quotes

--- scripts/eval_all.sh
+++ scripts/eval_all.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 for filename in $1"/*.pt;" do
-    python main.py --checkpoint_path vmf-$2$3-eval-all\
-               --src_language $2\
-               --trg_language $3\
-               --device cuda:$4\
-               --load_checkpoint_path $path$filename\
-               --use_checkpoint_args\
-               --unk_replace\
-               --mode eval\
-               -o mode\
-               -o unk_replace
+    python main.py\
+        --checkpoint_path "vmf-$2$3-eval-all"\
+        --src_language "$2"\
+        --trg_language "$3"\
+        --device "cuda:$4"\
+        --load_checkpoint_path "$path$filename"\
+        --use_checkpoint_args\
+        --unk_replace\
+        --mode eval\
+        -o mode\
+        -o unk_replace
     echo "Results for checkpoint:\n"$path$filename
 done
