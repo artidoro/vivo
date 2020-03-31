92d05cd5639bbc9f78dced8a77278b18075d6127
Ubuntu <ubuntu@ip-172-31-29-20.us-east-2.compute.internal>
2020-03-31 10:53:22 -0400

    eval_all

--- scripts/eval_all.sh
+++ scripts/eval_all.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
-for filename in $1"/*.pt;" do
-    /bin/bash "./eval.sh $path$filename $2 $3 $4"
-    echo "Results for checkpoint:\n"$path$filename
+for filename in ~/vivo/log/$1/*.pt
+do
+    echo "$filename"
+    #/bin/bash "~/vivo/scripts/eval.sh $filename $2 $3 $4"
+    python main.py\
+    --checkpoint_path "vmf-$2$3-eval-all"\
+    --src_language "$2"\
+    --trg_language "$3"\
+    --device "cuda:$4"\
+    --load_checkpoint_path "$filename"\
+    --use_checkpoint_args\
+    --unk_replace\
+    --mode eval\
+    -o mode\
+    -o unk_replace
+    
+    echo "Chckpoing for above result waas: $filename"
 done
+
